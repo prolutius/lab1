0b77d701339c3ff8efd413d3cb0f3123aa36bd90
prolutius <61651432+prolutius@users.noreply.github.com>
2020-03-20 13:43:48 +0300

    for pull

---
+++ out/build/x64-Debug/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/git/lab1/gtest/googlemock
+# Build directory: D:/git/lab1/out/build/x64-Debug/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ out/build/x64-Debug/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/git/lab1/gtest/googletest
+# Build directory: D:/git/lab1/out/build/x64-Debug/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
